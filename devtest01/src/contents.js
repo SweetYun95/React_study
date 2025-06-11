5328ad62d03d170627cdc7dda97955b93e18dae3
SweetYun95 <isr01154@naver.com>
2025-06-11 23:16:48 +0900

    25-06-11 23:16

---
+++ devtest01/src/contents.js
@@ -0,0 +1,7 @@
+// contents.js
+const contents = [
+   { id: 1, title: 'HTML', description: 'HTML is...' },
+   { id: 2, title: 'JS', description: 'JS is...' },
+   { id: 3, title: 'React', description: 'React is...' },
+]
+export default contents
